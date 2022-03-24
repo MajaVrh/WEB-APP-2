68b5850f15cecfbe92f08b200d0c367cd78daec1
MajaVrh <majavrh5@gmail.com>
2022-03-24 18:46:45 +0100

    zadaca 2

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-# web-app-2
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## WEB APLIKACIJE
+- Maja Vrh
+- JMBAG: 0082062903
+- Pošalji zahtjeve i prikaži ih u tablici 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <v-container 
+  
+    fluid
+    max-width="500px"
+    class="mt-16 background"
+    style="width: 50% !important"
+  >
+    <div class="text-h5 "
+     >Statistika imena</div>
+    <v-col :cols="12" justify="center" align="center">
+      <v-row class="inputDio">
+        <v-text-field
+          v-model="InputIme"
+          color="purple darken-2"
+          label="Upišite traženo ime"
+          required
+          class="w-50"
+        ></v-text-field>
+        <v-btn
+          @click="PrikaziStatistkuImena()"
+          color="purple darken-2"
+          fab
+          small
+          dark
+          class="ml-6"
+        >
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-row>
 
+      <v-data-table
+        :headers="headers"
+        :items="PopisImena"
+        class="elevation-1 mt-5"
+      ></v-data-table>
+    </v-col>
+  </v-container>
+</template>
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  //MV
+  name: "Home",
+  data() {
+    return {
+      InputIme: "",
+      PopisImena: [],
+      godineKorisnkika: "",
+      spolKorisnika: "",
+      imeKorisnika: "",
+      drzavaKorisnika: "",
+      headers: [
+        {
+          text: "Ime",
+          align: "start",
+          sortable: false,
+          value: "ime",
+        },
+        { text: "Država", value: "drzava" },
+        { text: "Godine", value: "godine" },
+        { text: "Spol", value: "spol" },
+      ],
+    };
+  },
+
+  methods: {
+    async PrikaziStatistkuImena() {
+      const podaciGodine = await axios.get(
+        "https://api.agify.io?name=" + this.InputIme
+      );
+
+      const podaciDrzava = await axios.get(
+        "https://api.nationalize.io?name=" + this.InputIme
+      );
+
+      const podaciSpol = await axios.get(
+        "https://api.genderize.io?name=" + this.InputIme
+      );
+let drz=""
+    podaciDrzava.data.country.forEach(element => {
+     drz += element.country_id+ " "+ "("+(element.probability*100).toFixed(0)+"%) " 
+    });
+
+
+
+      this.PopisImena.push({
+        ime: this.InputIme,
+        drzava: drz,
+        godine: podaciGodine.data.age,
+        spol: podaciSpol.data.gender + " "+"("+(podaciSpol.data.probability*100).toFixed(0) + "%) "  ,
+      });
+      this.InputIme=""
+    },
+  },
+};
+
+//Maja Vrh
 </script>
+
+<style scoped>
+.text-h5{color: rgb(88, 22, 150);}
+
+</style>
